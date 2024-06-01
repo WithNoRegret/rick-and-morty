09294990d074a04f5d8d4dba75ecf6173ea5c3a2
Sergey Pjankov <no.regret.ccg@gmail.com>
2024-06-01 13:02:51 +0300

    routing loading update

--- src/main.js
+++ src/main.js
@@ -3,18 +3,15 @@ import App from './App.vue'
 import './assets/main.css'
 
 import { createRouter, createWebHistory } from 'vue-router'
-import CharactersView from './views/CharactersView.vue'
-import CharacterCardView from './views/CharacterCardView.vue'
-import EpisodesView from './views/EpisodesView.vue'
 import Page404View from './views/Page404View.vue'
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: CharactersView },
-        { path: '/character/:id', component: CharacterCardView },
-        { path: '/episodes', component: EpisodesView },
+        { path: '/', component: () => import('./views/CharactersView.vue') },
+        { path: '/character/:id', component: () => import('./views/CharacterCardView.vue') },
+        { path: '/episodes', component: () => import('./views/EpisodesView.vue') },
         { path: '/:pathMatch(.*)*', component: Page404View }
     ]
 })
